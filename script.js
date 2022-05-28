01f1a7bd2dfb9dc6627f1c270f8ea81d904c1bee
omero2582 <iiomero2582ii@gmail.com>
2022-05-28 14:59:19 -0400

    Complete content space and right side branch
    Still need to finish last details on it, and review/edit code to remove
    suboptimal or unnecessary layout properties

---
+++ script.js
@@ -0,0 +1,47 @@
+function addProject(){
+    let outer = document.createElement('div');
+    outer.classList.add('project');
+
+    let statusBar = document.createElement('div');
+    statusBar.classList.add('status-bar');
+
+    let title = document.createElement('h2');
+    title.textContent = 'Title';
+
+    let description = document.createElement('p');
+    description.textContent ='this is a long paragraph description. Yes it is. Or it should be getting to that point. Aha.';
+
+    //buttons
+    let bottomRow = document.createElement('div');
+    bottomRow.classList.add('bottom-buttons');
+
+    let button1 = document.createElement('button');
+    let button2 = document.createElement('button');
+    let button3 = document.createElement('button');
+
+    button1.classList.add('favorite');
+    button2.classList.add('watch');
+    button3.classList.add('share');
+
+    bottomRow.appendChild(button1);
+    bottomRow.appendChild(button2);
+    bottomRow.appendChild(button3);
+    //
+
+    
+    outer.appendChild(statusBar);
+    outer.appendChild(title);
+    outer.appendChild(description);
+    outer.appendChild(bottomRow)
+
+
+
+
+    const projects = document.querySelector('.my-projects');
+    projects.appendChild(outer);
+
+}    
+
+for (let i=0; i<8; i++){
+    addProject();
+}
